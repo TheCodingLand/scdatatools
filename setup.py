d04c4593f5baec6f996d10c289c38c4a12ff1ef3
Ventorvar <ventorvar@gmail.com>
2021-04-02 22:27:28 -0400

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -82,6 +82,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/scmodding/frameworks//scdatatools',
-    version='0.1.6',
+    version='0.1.7',
     zip_safe=True,
 )
